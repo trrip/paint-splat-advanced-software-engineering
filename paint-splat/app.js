fac25b8ee8a0cffd9cd007de7d26ac2409334bf2
joshuacassidy <joshcassidy79@gmail.com>
2020-10-22 12:20:30 +0100

    set up page structure

--- paint-splat/app.js
+++ paint-splat/app.js
@@ -6,8 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var boardRouter = require('./routes/board');
+var resultsRouter = require('./routes/results');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/game', boardRouter);
+app.use('/results', resultsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
