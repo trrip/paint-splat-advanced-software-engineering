f00d3d161b5bfd4ad4104d39fb238bae7e33e6f1
Nilesh Choudhary <choudhary.nilesh@media4u.com>
2020-10-29 17:17:22 +0000

    Made queue and initial pathing of the box better

--- paint-splat/socket/connection-handler.js
+++ paint-splat/socket/connection-handler.js
@@ -1,138 +1,129 @@
- 
-GROUP_LENGTH = 2;
- 
- class ConnectionHandler{
-    constructor(){
+GROUP_LENGTH = 3;
 
-        this.queueManager = new Queue();
+class ConnectionHandler {
+  constructor() {
+    this.queueManager = new Queue();
+  }
+  listen = (appServer) => {
+    const options = {
+      /* ... */
+    };
+    const io = require("socket.io")(appServer, options);
 
-    }
-    listen = (appServer) => {
-        
-        const options = { /* ... */ };
-        const io = require('socket.io')(appServer, options);
+    io.on("connection", (socket) => {
+      console.log("New socket urser is connected");
 
-            
-        io.on('connection', socket => { 
-            console.log("New socket urser is connected");
+      this.queueManager.addMemberToQueue(socket, io);
+      // socket.on([...somevar], ...);
 
-            this.queueManager.addMemberToQueue(socket,io)
-                // socket.on([...somevar], ...);
+      //
+      socket.on("disconnect", () => {
+        console.log("user disconnected");
+      });
 
-                //
-                socket.on('disconnect', () => {
-                    console.log('user disconnected');
-            });
-
-
-        socket.on("gameCom", (socketMessage) => {
-             let tempChannel = socketMessage.roomId;
-            io.sockets.in(tempChannel).emit('gameCom', {roomId : tempChannel , message : "some new edit messgae "});
-        })
-
-        }
-        );
-
-    }
-
+      socket.on("gameCom", (socketMessage) => {
+        let tempChannel = socketMessage.roomId;
+        io.sockets.in(tempChannel).emit("gameCom", {
+          roomId: tempChannel,
+          message: socketMessage.message,
+        });
+      });
+    });
+  };
 }
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.round(Math.random() * (max - min + 1)) + min;
+}
 
+function createRandomStream() {
+  let xSteps = [];
+  let ySteps = [];
 
-
-
+  for (let i = 0; i < 100; i++) {
+    movex = Math.round(getRandomInt(-1, 1));
+    movey = Math.round(getRandomInt(-1, 1));
+    xSteps.push(movex);
+    ySteps.push(movey);
+  }
+  return {
+    xSteps: xSteps,
+    ySteps: ySteps,
+  };
+}
 
 class GameSession {
+  constructor(members, io) {
+    // this.sessionId = "";
+    // this.player = [];
+    this.uniqueName = members[0].id;
 
-    constructor(members, io) {
-        
-        // this.sessionId = "";
-        // this.player = [];
-        this.uniqueName = members[0].id; 
-
-        for (let i of members){
-            //create a unique name here -- 
-            i.join(this.uniqueName);
-        }
-        
-        io.sockets.in(this.uniqueName).emit('gameCom', {roomId : this.uniqueName , message : "some messgae "});
-
-        io.sockets.in(this.uniqueName).on("gameCom", (socketMessagee) => {
-            console.log("message incoming");
-            io.sockets.in(this.uniqueName).emit('gameCom', {roomId : this.uniqueName , message : "message recieved messgae "} );
-
-        });
-
-
-        
-
-        // io.on("gameCom", (socketMessagee) =>{
-        //     console.log("we should be getting this");
-        //     // socketMessagee suppose this coordiates
-        //     // io.to("someUniqueName").emit("gameCom","new message");
-        //     io.sockets.in("someUniqueName").emit("gameCon","messgahe receieved")
-        //     // if(valid_coordintes(socketMessagee)) {
-        //     //     io.emit("gameCon", `success + coordinates`); // send the coordinates back to render on other screen
-        //     // } else {
-        //     //     io.emit("gameCon", "you missed");
-        //     // }
-
-
-        // });
-        // passing throgh 
-        // score 
-
-        // splashes [ location, player]. 
+    for (let i of members) {
+      //create a unique name here --
+      i.join(this.uniqueName);
     }
-  /*
-group of players : max
-session id 
+    let randomVel = [1, -1];
 
+    // list of coordinates that square will follow
+    // keep game start message
+    // let coordinates = createRandomStream();
+    let startX = getRandomInt(30, 390);
+    let startY = getRandomInt(30, 390);
 
-*/ }
+    let initialVelocityX = Math.random() < 0.5 ? -1 : 1;
+    let initialVelocityY = Math.random() < 0.5 ? -1 : 1;
+    io.sockets.in(this.uniqueName).emit("gameCom", {
+      roomId: this.uniqueName,
+      message: {
+        status: 200,
+        startX: startX,
+        startY: startY,
+        initialVelX: initialVelocityX,
+        initialVelY: initialVelocityY,
+      },
+    });
+    io.sockets.in(this.uniqueName).on("gameCom", (socketMessagee) => {
+      console.log("message incoming");
+      io.sockets.in(this.uniqueName).emit("gameCom", {
+        roomId: this.uniqueName,
+        message: socketMessagee.message,
+      });
+    });
+
+    io.sockets.on("disconnect", () => {
+      console.log("user disconnected");
+    });
+  }
+}
 
 class Queue {
-    // socketId = 0
-    constructor(){   
-        this.waitingMembersQueue = []; //members that are waiting in the queue. //waiting room 
+  // socketId = 0
+  constructor() {
+    this.waitingMembersQueue = []; //members that are waiting in the queue. //waiting room
+    this.gameSessions = [];
+  }
 
-        this.gameSessions = [];
+  removeMemberFromQueue = () => {};
+
+  addMemberToQueue = (socketMember, io) => {
+    // we can generate the unique session is and make a game session out of it
+
+    this.waitingMembersQueue.push(socketMember);
+
+    if (this.waitingMembersQueue.length == GROUP_LENGTH) {
+      console.log("we are making a queue " + this.waitingMembersQueue.length);
+      this.gameSessions.push(
+        new GameSession([...this.waitingMembersQueue], io)
+      );
+
+      console.log("starting a new game");
+
+      this.waitingMembersQueue = [];
+      //   console.log("e" + this.waitingMembersQueue);
     }
-
-    removeMemberFromQueue = () =>{}
-
-    addMemberToQueue = (socketMember, io ) =>{
-        // we can generate the unique session is and make a game session out of it
-
-        this.waitingMembersQueue.push(socketMember);
-
-        console.log("the player " + socketMember);
-
-        if(this.waitingMembersQueue.length == GROUP_LENGTH){
-            console.log("we are making a queue " + this.waitingMembersQueue.length)
-            this.gameSessions.push( new GameSession([...this.waitingMembersQueue],io))
-            
-            this.waitingMembersQueue = []
-            console.log("we are making a queue" + this.waitingMembersQueue);
-
-            //create a game session
-            
-            // add those t game session
-            
-            // pop them from the queue 
-
-            // start the session.
-            
-
-        }
-        // else{
-        //     // do a loop and find a game session to add them to or look at last one in session list
-        //         // pass 
-        // }
-    }
-
+  };
 }
-var handler = new  ConnectionHandler();
-module.exports =  handler;
-
-
+var handler = new ConnectionHandler();
+module.exports = handler;
