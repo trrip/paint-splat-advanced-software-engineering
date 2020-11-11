012a8710edb35b426142f2cea9593cf12427ead5
Nilesh Choudhary <choudhary.nilesh@media4u.com>
2020-11-11 16:22:59 +0000

    changed path

--- .circleci/code_deploy.sh
+++ .circleci/code_deploy.sh
@@ -1,7 +1,7 @@
 # git pull
 # stop the existing process
 # start a new process.
-
+cd ~/ASW/paint-splat-advanced-software-engineering/
 git pull
 pm2 stop paint_splat
 pm2 start ~/ASW/paint-splat-advanced-software-engineering/paint-splat/bin/www --name paint_splat
