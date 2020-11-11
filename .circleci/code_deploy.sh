14de3f8bebcb9bd1d421ebf9cc6249d28be8bc09
Nilesh Choudhary <choudhary.nilesh@media4u.com>
2020-11-11 16:20:51 +0000

    modified deploy code

--- .circleci/code_deploy.sh
+++ .circleci/code_deploy.sh
@@ -3,5 +3,5 @@
 # start a new process.
 
 git pull
-pm2 stop paint-splat
-pm2 start ~/ASW/paint-splat-advanced-software-engineering/paint-splat/bin/www
+pm2 stop paint_splat
+pm2 start ~/ASW/paint-splat-advanced-software-engineering/paint-splat/bin/www --name paint_splat
