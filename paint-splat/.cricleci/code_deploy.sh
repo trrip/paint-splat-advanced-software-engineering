f9b786e9862bcecdaefef92d53062c7a0d6560a4
joshuacassidy <joshcassidy79@gmail.com>
2020-11-17 14:29:44 +0000

    fixed deployment error

--- paint-splat/.cricleci/code_deploy.sh
+++ paint-splat/.cricleci/code_deploy.sh
@@ -2,6 +2,6 @@
 # stop the existing process
 # start a new process.
 
-git pull
+git pull --rebase
 pm2 stop paint-splat
 pm2 start ~/ASW/paint-splat-advanced-software-engineering/paint-splat/bin/www
